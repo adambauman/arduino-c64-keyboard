7a9f1597d299e110426fb2af0485fb51fd0f392e
adambauman <adam.bauman@geeksquad.com>
2016-04-14 17:18:09 -0500

    Checkpoint

--- arduino-c64_keyboard.ino
+++ arduino-c64_keyboard.ino
@@ -1,24 +1,28 @@
 // BauTek C64 PiCase Keyboard Controller
 //
 
-#include <Keypad.h>
-
 void setup() {
 const byte rows = 8;
 const byte cols = 8;
 
-char key_map[rows][cols] = {
-//  {1,LEFTARROW,CTRL,RUNSTOP,SPACE,COMMODORE,Q,2},
-//  {"3","w","a","leftShift","z","s","e","4"},
-//  {"5","r","d","x","c","f","t","6"},
-//  {"7","y","g","v","b","h","u","8"},
-//  {"9","i","j","n","m","k","o","0"},
-//  {"+","p","l",",",".",":","@","-"},
-//  {"$","*",";","/","rightShift","=","upArrow","home"},
-//  {"del","return","crsrHor","crsrVert","f1","f3","f5","f7"}
-//};
+boolean shiftPressed = false;
+boolean altPressed = false;
+boolean ctrlPressed = false;
+boolean capsLockState = false;
 
-byte 
+// Set to direct ASCII values of keys to send
+byte key_map_unmodified[rows][cols] = {
+  {49, 96, 128, 177, 32, 130, 113, 50},
+  {51, 119, 97, 129, 122, 115, 101, 52},
+  {53, 114, 100, 120, 99, 102, 116, 54},
+  {55, 121, 103, 118, 98, 104, 117, 56},
+  {57, 105, 106, 110, 109, 107, 111, 48},
+  {43, 112, 108, 44, 46, 58, 64, 45},
+  {36, 42, 59, 47, 133, 61, 94, 179},
+  {212, 176, 215, 217, 194, 196, 198, 200}
+};
+
+// Restore (I8) = 178 (backspace)
 
 boolean key_status[rows][cols] = {
   {false,false,false,false,false,false,false,false},
@@ -30,15 +34,6 @@ boolean key_status[rows][cols] = {
   {false,false,false,false,false,false,false,false},
   {false,false,false,false,false,false,false,false},
 };
-
-
-
-
-//byte rowPins[rows] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-//byte colPins[cols] = {8, 7, 6}; //connect to the column pinouts of the keypad
-//Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
-
-
 
 
 }
